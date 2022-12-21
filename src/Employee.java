785e9f9fad292d00e13729f3e1a36075356bfcf0
aliilhann <alirizailhan89@gmail.com>
2022-12-21 00:20:01 -0600

    Employee Project, add a few more pages

---
+++ src/Employee.java
@@ -0,0 +1,6 @@
+public class Employee {
+
+    int age;
+    String name;
+    String department;
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola amigos. Here is the git practice project");
+
+        Employee employee=new Employee();
+        employee.age=30;
+        employee.name="Mark";
+        employee.department="Accountant";
+    }
+}
